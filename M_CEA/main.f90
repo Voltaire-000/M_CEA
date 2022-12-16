d2214272628cc6e852795978f3edfcb0215a4c7e
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-12-16 14:02:03 +0700

    add example.inp

--- M_CEA/main.f90
+++ M_CEA/main.f90
@@ -11,5 +11,15 @@ program hello
     real*8 xi, xln
     real*8 DLOG
     
+    Save caseok, ensert, ex, i, inc, infile, Pfile, iof, j, ln, n, ofile, prefix, readok, xi, xln
     
+    Write(*, 99001)
+    Read(*, 99002)prefix
+    
+    
+99001 FORMAT(//'Enter input file name'//)
+99002 FORMAT(a)
+99003 FORMAT(1X, A16, 'Inserted')
+99004 FORMAT (/'Warning', A16, 'Not found for insertion')
+!99005 FORMAT(lx, lp, 20E12.4)      
     end program
