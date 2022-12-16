06db041bf00c78b8cccbc9f2d0284e252035694f
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-12-16 13:44:56 +0700

    add cea.inc variables

--- M_CEA/main.f90
+++ M_CEA/main.f90
@@ -1,8 +1,15 @@
 program hello
     implicit none
+    include 'cea.inc'
     
     character*15 ensert(20)
     character*200 infile, ofile
     character*196 prefix
+    logical caseok, ex, readok
+    integer i, inc, iof, j, ln, n
+    integer index
+    real*8 xi, xln
+    real*8 DLOG
+    
     
     end program
